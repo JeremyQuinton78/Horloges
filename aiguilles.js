d0d6acf14dd77de3df1886f7e17c9d99e5aceb62
jeremy <jeremy.quinton@edu.itescia.fr>
2018-03-20 10:00:34 +0100

    aiguilles continu

--- aiguilles.js
+++ aiguilles.js
@@ -5,17 +5,18 @@ function refreshData(){
 		var Hours = d.getHours();
 		var Minutes = d.getMinutes();
 		var Seconds = d.getSeconds();
+		var MilliS = d.getMilliseconds();
 
 		var degH = ((Hours+Minutes/60)*360/12) ;
 		var degM = Minutes*360/60;
-		var degS = Seconds*360/60;
+		var degS = (Seconds+MilliS/1000)*360/60;
+
 
 		document.getElementById("hourPointer").style.transform = "rotate("+degH+"deg)" ;
 		document.getElementById("minutePointer").style.transform = "rotate("+degM+"deg)" ;
 		document.getElementById("secondPointer").style.transform = "rotate("+degS+"deg)" ;
 
-		setTimeout(refreshData, 1000) ;
+		setTimeout(refreshData, 0) ;
 
 }
-
 refreshData();
