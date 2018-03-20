45a30d0878ce518d386f683c405fa7ecf70ca54e
jeremy <jeremy.quinton@edu.itescia.fr>
2018-03-20 15:01:20 +0100

    select option

--- aiguilles.js
+++ aiguilles.js
@@ -7,10 +7,12 @@ function refreshData(){
 		var Seconds = d.getSeconds();
 		var MilliS = d.getMilliseconds();
 
+		var selectVille = document.getElementById("ville") ; 
+		console.log(selectVille.value);
+
 		var degH = (Hours+Minutes/60+Seconds/3600)*360/12;
 		var degM = (Minutes+Seconds/60+MilliS/60000)*360/60;
 		var degS = (Seconds+MilliS/1000)*360/60;
-
 
 		document.getElementById("hourPointer").style.transform = "rotate("+degH+"deg)" ;
 		document.getElementById("minutePointer").style.transform = "rotate("+degM+"deg)" ;
@@ -36,15 +38,5 @@ function refreshData(){
 }
 refreshData();
 
-    /* click sur l'icone burger menu, ca active la classe change */
-    function myFunction(x) {
-		x.classList.toggle("change");
-	
-		/* variable monElement = css du burgerMenu */
-		var monElement = document.getElementById("burgerMenu");
-		/* condition si la liste du burgerMenu est affichée on cache, sinon on affiche */
-		if (monElement.style.display == "block")
-			monElement.style.display="none";
-		else
-		   monElement.style.display="block";
-	}
+
+
